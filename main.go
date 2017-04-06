a4e44a35e4ed7dfa057b53e35ebcde0b1657d8bc
Daniel Bonkowski <bonko@jimdo.com>
2017-04-06 11:18:12 +0200

    make duration until light goes off configurable

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/Bonko/hue"
@@ -11,7 +12,6 @@ import (
 
 func main() {
 	log.Println("listening..")
-	//	sleepTimer()
 	http.HandleFunc("/", rootHandler)
 	http.HandleFunc("/sleepTimer", startSleepTimer)
 	if err := http.ListenAndServe(":9091", nil); err != nil {
@@ -21,7 +21,21 @@ func main() {
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	html := `<a href="/sleepTimer">sleep timer</a>`
+	html := `
+	<html>
+	<form action="/sleepTimer">
+	  <label>Sleeptimer duration (minute):
+	    <select name="duration" size="1">
+		  <option>1</option>
+		  <option>5</option>
+		  <option>10</option>
+		  <option selected>20</option>
+		  <option>30</option>
+		</select>
+		<input type="submit" value="Go">
+	  </label>
+	</form>
+	</html>`
 	fmt.Fprintf(w, html)
 }
 
@@ -32,7 +46,7 @@ func auth() *hue.Bridge {
 		log.Fatal(err)
 	}
 	if !b.IsPaired() {
-		log.Println("program not paired with bridge")
+		log.Println("program not paired with bridge, pairing now")
 		// link button must be pressed before calling
 		if err := b.Pair(); err != nil {
 			log.Println("pairing failed")
@@ -43,10 +57,18 @@ func auth() *hue.Bridge {
 }
 
 func startSleepTimer(w http.ResponseWriter, r *http.Request) {
-	go sleepTimer()
-	fmt.Fprintf(w, "Started sleepTimer")
+	r.ParseForm()
+	d := r.Form["duration"][0]
+	duration, err := strconv.Atoi(d)
+	if err != nil {
+		log.Fatal("cannot  convert  duration to int: %s", err)
+	}
+
+	go sleepTimer(time.Duration(duration))
+	fmt.Fprintf(w, "Started sleepTimer (duration: %d minutes)", duration)
 }
-func sleepTimer() {
+
+func sleepTimer(duration time.Duration) {
 	b := auth()
 	nk, err := b.Lights().Get("Nachtkaestchen")
 	if err != nil {
@@ -58,19 +80,19 @@ func sleepTimer() {
 	nk.Set(&hue.State{
 		Brightness: brightness,
 	})
-	remaining_time := 20 * time.Minute
+	remaining_time := duration * time.Minute
+	interval := remaining_time / 10
 	for remaining_time > 0 && brightness > 25 {
-		interval := 2 * time.Minute
 		log.Println("Sleeping", interval)
 		time.Sleep(interval)
 		brightness = brightness - 25
-		log.Println("Decreasing brightness to", brightness)
 		nk.Set(&hue.State{
 			Brightness: brightness,
 		})
-		log.Println(nk.State.Brightness)
+		log.Println("Decreased brightness to:", nk.State.Brightness, "expected:", brightness)
 		remaining_time = remaining_time - interval
 		log.Println("Remaining Time:", remaining_time)
 	}
+	log.Println("Turning light off")
 	nk.Off()
 }
